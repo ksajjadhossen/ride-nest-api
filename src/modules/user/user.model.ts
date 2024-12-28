5ec313548594042491b5bed96019af8d3d5f210e
ksajjadhossen <sk.sajjad4207@gmail.com>
2024-12-28 22:11:03 +0600

    user model created done.

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -1,6 +1,43 @@
-import { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import { IUser } from "./user.interface";
 
 const userSchema = new Schema<IUser>({
-	name: { type: String },
+	name: {
+		type: String,
+		required: [true, "Name is required"],
+	},
+	id: {
+		type: String,
+		required: [true, "ID is required"],
+	},
+	email: {
+		type: String,
+		required: [true, "Email is required"],
+		match: [/\S+@\S+\.\S+/, "Please provide a valid email address"],
+	},
+	password: {
+		type: String,
+		required: [true, "Password is required"],
+		minlength: [6, "Password must be at least 6 characters long"],
+	},
+	phone: {
+		type: String,
+		required: [true, "Phone number is required"],
+	},
+	address: {
+		type: String,
+		required: [true, "Address is required"],
+	},
+	role: {
+		type: String,
+		required: [true, "Role is required"],
+	},
+	isDeleted: {
+		type: Boolean,
+		required: [true, "isDeleted field is required"],
+	},
 });
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export { IUser, User };
