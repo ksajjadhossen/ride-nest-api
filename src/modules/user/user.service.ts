f2e0da1c76d94392c389bc469f7a8d7e19bd44b2
ksajjadhossen <sk.sajjad4207@gmail.com>
2025-01-19 23:43:47 +0600

    invalid id logic on user find created on function

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,14 +1,20 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 import { TUser } from "./user.interface";
 import { User } from "./user.model";
-
 const getAllUsers = async () => {
 	const result = User.find();
 	return result;
 };
 
 const updateUser = async (payload: string, data: Partial<TUser>) => {
+	const userData = await User.findOne({ _id: payload });
+	if (!userData) {
+		throw new AppError(httpStatus.NOT_FOUND, "User not found");
+	}
+	console.log("here is the userData", userData);
 	if (!payload || !data) {
-		throw new Error("Data could not found");
+		throw new AppError(httpStatus.NOT_FOUND, "User not found");
 	}
 	const result = await User.findByIdAndUpdate(payload, data, { new: true });
 	return result;
